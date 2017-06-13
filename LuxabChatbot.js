08ff11ebc837c2a21947c2e13e6d8e8ddacdcf93
Duncan <dklug@ucmerced.edu>
2017-06-13 23:15:24 +0000

    Implemented Rise of Nations taunts

--- LuxabChatbot.js
+++ LuxabChatbot.js
@@ -10,10 +10,20 @@ const token = 'TOKEN GOES HERE';
 // The username of your bot
 const username = 'USERNAME GOES HERE';
 
+// The filepath of the bot, used for playing taunt files
+const path = 'FILE PATH GOES HERE /LuxabChatbot/taunts/';
+
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
 bot.on('ready', () => {
   console.log('I am ready!');
+  bot.user.setPresence({
+    'status': 'online',
+    'afk': false,
+    'game': {
+      'name': 'LuxabChatbot'
+    }
+  });
 });
 
 //message.channel.send('example text to speech message', {'tts':true});
@@ -22,37 +32,71 @@ bot.on('ready', () => {
 bot.on('message', message =>
 {
   const cont = message.content.toLowerCase();
-  const chnl = message.channel;
+  const txtchnl = message.channel;
+
+  function taunt(num)
+  {
+    if (message.member.voiceChannel)
+    {
+      message.member.voiceChannel.join()
+        .then(connection =>
+          { // Connection is an instance of VoiceConnection
+          const toPlay = connection.playFile(path+num+'.wav');
+          txtchnl.send('('+num+')');
+          toPlay.on('error', e =>
+          {
+            // Catch any errors that may arise
+            console.log(e);
+          });
+
+          toPlay.on('end',()=>{
+            //disconnect once sound is played
+            message.member.voiceChannel.leave();
+          });
+        })
+        .catch(console.log);
+    }
+  }
+
   if (message.author.username!==username)
   {
+    for (i = 1; i<101; i++)
+    {
+      if (cont==i)
+      {
+        //console.log(i);
+        taunt(i);
+      }
+    }
+
     if (cont.includes('literal'))
     {
-      chnl.send('*literally*');
+      txtchnl.send('*literally*');
     }
 
     if (cont.includes(':rosen:'))
     {
-      chnl.send('http://imgur.com/Khhnr0X');
+      txtchnl.send('http://imgur.com/Khhnr0X');
     }
 
     if (cont.includes('think'))
     {
-      chnl.send('http://imgur.com/a/6BXDO');
+      txtchnl.send('http://imgur.com/a/6BXDO');
     }
 
     if (cont.includes('cat'))
     {
-      chnl.send('http://imgur.com/a/fvoZS');
+      txtchnl.send('http://imgur.com/a/fvoZS');
     }
 
     if (cont.includes('prawn')||cont.includes('srars'))
     {
-      chnl.send('https://www.youtube.com/watch?v=5mEJbX5pio8');
+      txtchnl.send('https://www.youtube.com/watch?v=5mEJbX5pio8');
     }
 
     if (cont.includes('jiffy')||cont.includes('jeffrey'))
     {
-      chnl.send('http://imgur.com/a/BriTS');
+      txtchnl.send('http://imgur.com/a/BriTS');
     }
 
     //Detects thinking emoji and spams it a bit
@@ -60,7 +104,7 @@ bot.on('message', message =>
     {
       for (i = 0; i<4; i++)
       {
-        chnl.send(':thinking:')
+        txtchnl.send(':thinking:')
       }
     }
 
