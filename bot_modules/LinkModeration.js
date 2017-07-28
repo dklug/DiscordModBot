661d013d9aaf77d36c795e47162d1f6979ef3a5a
Luxab <duncanklug@outlook.com>
2017-07-27 17:49:32 -0700

    Merge branch 'master' of https://github.com/Luxab/LuxabChatbot

--- bot_modules/LinkModeration.js
+++ bot_modules/LinkModeration.js
@@ -5,24 +5,25 @@ exports.help = "LinkModeration.js - Sets a cooldown for how many links can be po
 // Create an event listener for messages
 bot.on('message', message =>
 {
-  if (message.author.username!==botusername)
+  var mauthor = message.author;
+  if (mauthor.username!==botusername)
   {
     const cont = message.content.toLowerCase();
     const txtchnl = message.channel;
     //Link Cooldown function
     if (cont.includes("http"))
     {
-        if (!message.author.linkCount)
-        {message.author.linkCount = 0;}
-        if (message.author.linkCount>4)
+        if (!mauthor.linkCount)
+        {mauthor.linkCount = 0;}
+        if (mauthor.linkCount>4)
         {
           message.delete();
           txtchnl.send("Calm down");
         }
         else
         {
-          message.author.linkCount++;
-          setTimeout(function(message){message.author.linkCount--;},500000);
+          mauthor.linkCount++;
+          setTimeout(function(message){author.linkCount--;},500000);
         }
     }
   }
